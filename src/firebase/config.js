c18ab6c41bbf78d33bd79b08b812bb9ee063a0a5
Aaron Thomas <84493696+arxhr007@users.noreply.github.com>
2025-03-23 14:36:58 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ2xe6hdBHxOCpb5_cgp6njwTv5hTY888",
-  authDomain: "nyxora-7aa0d.firebaseapp.com",
-  projectId: "nyxora-7aa0d",
-  storageBucket: "nyxora-7aa0d.appspot.com", // Fixed storageBucket URL
-  messagingSenderId: "250070108630",
-  appId: "1:250070108630:web:b94a6e4af5fee82723fc77"
+config
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); // Initialize Firestore
+export const db = getFirestore(app); 
